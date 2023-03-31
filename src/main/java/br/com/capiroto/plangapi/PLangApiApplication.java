dbbde012dfda0a2404f79b6492fef06b3d9edee7
diegoluan <diegoluan@weg.net>
2023-03-31 09:05:40 -0300

    first commit

---
+++ src/main/java/br/com/capiroto/plangapi/PLangApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.capiroto.plangapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PLangApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PLangApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/capiroto/plangapi/PLangApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capiroto.plangapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PLangApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
